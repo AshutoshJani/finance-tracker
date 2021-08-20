cfc25366f63d2dfdfba22db12517015804ea4490
AshutoshJani <ashutosh.jani.42@gmail.com>
2021-08-20 13:09:47 +0530

    Added a Stock table and IEX Cloud Sandbox API to get real-time Stock prices

---
+++ app/models/stock.rb
@@ -0,0 +1,11 @@
+class Stock < ApplicationRecord
+
+    def self.new_lookup(ticker)
+        client = IEX::Api::Client.new(
+            publishable_token: Rails.application.credentials.iex_cloud[:sandbox_api_key],
+            endpoint: 'https://sandbox.iexapis.com/v1'
+          )
+        client.price(ticker)
+    end
+
+end
