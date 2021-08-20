6c97ffa184745faeb7d71161d19153cab6077c03
AshutoshJani <ashutosh.jani.42@gmail.com>
2021-08-20 18:19:03 +0530

    Added Exception handling and proper display for the IEX API response

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,7 +5,11 @@ class Stock < ApplicationRecord
             publishable_token: Rails.application.credentials.iex_cloud[:sandbox_api_key],
             endpoint: 'https://sandbox.iexapis.com/v1'
           )
-        client.price(ticker)
+        begin
+            new(ticker: ticker, name: client.company(ticker).company_name, last_price: client.price(ticker))
+        rescue => exception
+            return nil
+        end
     end
 
 end
