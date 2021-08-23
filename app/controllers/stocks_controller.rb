6e0f60a58695042fa9b34e81c8d1f10a34ae10f3
AshutoshJani <ashutosh.jani.42@gmail.com>
2021-08-23 12:18:18 +0530

    Completed adding Ajax request and JS response to the portfolio page

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -8,12 +8,18 @@ class StocksController < ApplicationController
                     format.js { render partial: 'users/result.js' }
                 end
             else
-                flash.now[:alert] = "Please enter a valid ticker symbol to search"
-                # render 'users/my_portfolio'
+                respond_to do |format|
+                    flash.now[:alert] = "Please enter a valid ticker symbol to search"
+                    format.js { render partial: 'users/result.js' }
+                    # render 'users/my_portfolio'
+                end
             end
         else
-            flash.now[:alert] = "Please enter a ticker symbol to search"
-            # render 'users/my_portfolio'
+            respond_to do |format|
+                flash.now[:alert] = "Please enter a ticker symbol to search"
+                format.js { render partial: 'users/result.js' }
+                # render 'users/my_portfolio'
+            end
         end
     end
     
