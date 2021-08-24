c6722c7e9ee97881c39fcf79ca063b5631a7d757
AshutoshJani <ashutosh.jani.42@gmail.com>
2021-08-24 11:35:43 +0530

    Added user full name for the database along with the ability to edit user details

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,9 @@ class User < ApplicationRecord
     under_stock_limit? && !already_tracked?(ticker)
   end
 
+  def full_name
+    return "#{first_name} #{last_name}" if first_name || last_name
+    "Anonymous"
+  end
+
 end
